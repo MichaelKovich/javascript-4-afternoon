55a1b1a07e68bb8f51c33e148de9e4fc8ad1760f
Michael Kovich <kovich@michaelkovich.com>
2018-03-15 14:23:30 -0500

    Part I complete. 26 specs, 0 failures.

--- closures.js
+++ closures.js
@@ -1,4 +1,4 @@
-/* 
+/*
   Once you complete a problem, refresh ./closures.html in your browser and check to see if the problem's test(s) are passing.
   Passed tests will be indicated by a green circle.
   Failed tests will be indicated by a red X.
@@ -6,38 +6,34 @@
   You can refresh the page at any time to re-run all the tests.
 */
 
-////////// PROBLEM 1 //////////
+// //////// PROBLEM 1 //////////
 
 // Do not edit the code below.
 function outer() {
-  var name = 'Tyler';
-  return function() {
-    return 'The original name was ' + name;
+  const name = 'Tyler';
+  return function () {
+    return `The original name was ${name}`;
   };
 }
 // Do not edit the code above.
-  
-/* 
+
+/*
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
 
+const inner = outer();
 
+// Once you do that, invoke inner.
 
-//Once you do that, invoke inner.
+inner();
 
-//Code Here
-
-
-
-////////// PROBLEM 2 //////////
+// //////// PROBLEM 2 //////////
 
 // Do not edit the code below.
 function callFriend(name) {
   function dial(number) {
-    return 'Calling ' + name + ' at ' + number
+    return `Calling ${name} at ${number}`;
   }
   return dial;
 }
@@ -46,110 +42,109 @@ function callFriend(name) {
 /*
   Above you're given a callFriend function that returns the dial function.
   Store the result of invoking callFriend in a variable named callJake.
-  
-  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248' 
+
+  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248'
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+const callJake = callFriend('Jake');
+callJake('435-555-9248');
 
-
-
-////////// PROBLEM 3 //////////
+// //////// PROBLEM 3 //////////
 
 /*
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter() {
+  let start = 0;
+  return function inner() {
+    return (start += 1);
+  };
+}
 
+const count = makeCounter();
+count(); // 1
+count(); // 2
+count(); // 3
+count(); // 4
 
-
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
-
-
-
-////////// PROBLEM 4 //////////
+// //////// PROBLEM 4 //////////
 
 /*
   Inside the function called counterFactory return two functions that implement up/down counter.
   The first function is called inc, this function is responsible for incrementing the value once and returning the updated value.
   The second function is called dec, this function is responsible for decrementing the value by one and returning the updated value.
   You will need to use the module pattern to achieve this.
-  Information on the module pattern available here: 
+  Information on the module pattern available here:
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
 */
 
 function counterFactory(value) {
-  // Code here.
-
   return {
-
+    inc() {
+      return (value += 1);
+    },
+    dec() {
+      return (value -= 1);
+    },
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc(); // 11
+counter.inc(); // 12
+counter.inc(); // 13
+counter.dec(); // 12
 
-
-
-////////// PROBLEM 5 //////////
+// //////// PROBLEM 5 //////////
 
 /*
   Inside the motivation function create another function called message that will return the welcome text with the firstname and lastname.
-  The final message should say "You're doing awesome, keep it up firstname lastname." 
+  The final message should say "You're doing awesome, keep it up firstname lastname."
   (Hint: don't forget to have a space between the firstname and lastname and a period at the end of the sentence.)
 */
 
-function motivation( firstname, lastname ) {
-  var welcomeText = "You're doing awesome, keep it up";
-
-  // code message function here.
-
-  //Uncommment this to return the value of your message function
-  //return message;
+function motivation(firstname, lastname) {
+  const welcomeText = "You're doing awesome, keep it up";
+  return function () {
+    const message = `${welcomeText} ${firstname} ${lastname}.`;
+    return message;
+  };
 }
 
-var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
+const greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
 
-
-
-////////// PROBLEM 6 //////////
+// //////// PROBLEM 6 //////////
 
 /*
   Inside the module's return object create a publicMethod function that invokes privateMethod (return the result).
   Invoke this by calling module.publicMethod(); outside the module scope
 */
 
-var module = (function() {
-  var person = {
-    name: "phillip",
+const module = (function () {
+  const person = {
+    name: 'phillip',
     age: 29,
-    location: "Utah"
+    location: 'Utah',
   };
 
-  function privateMethod(){
-    return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
+  function privateMethod() {
+    return `Hi, I'm ${person.name}, age ${person.age} from ${person.location}`;
   }
 
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod() {
+      return privateMethod();
+    },
   };
-})();
+}());
 
+module.publicMethod();
 
-
-////////// PROBLEM 7 //////////
+// //////// PROBLEM 7 //////////
 
 /*
   Here we have a function named secretNumber that has a secret number.
@@ -159,17 +154,22 @@ var module = (function() {
 */
 
 function secretNumber() {
-  var secret = 143;
+  let secret = 143;
 
   return {
-    // Code here
+    addToSecret(num) {
+      secret += num;
+      return secret;
+    },
+    takeAwayFromSecret(num) {
+      secret -= num;
+      return secret;
+    },
   };
 }
 
+// //////// PROBLEM 8 //////////
 
-
-////////// PROBLEM 8 //////////
-  
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
   What we need to do is console.log(i) so that it logs like so:
@@ -182,13 +182,14 @@ function secretNumber() {
 
   However, because each call to console.log occurs after the loop has finished, the value of i has changed before the console.log executes.
   We'll need to use a closure to preserve a reference to i at the time of execution.
-  
+
   Fix the code below to log the desired output.
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
+  for (let i = 0; i <= 5; i++) {
+    // Change "var" to "let" and the problem is solved. ES6 ftw.
+    setTimeout(() => {
       console.log(i);
     }, i * 1000);
   }
